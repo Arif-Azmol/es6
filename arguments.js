2b535ff4dc244a0fc75e878e953bacbffff48185
Arif Azmol <77983737+Arif-Azmol@users.noreply.github.com>
2024-07-10 11:28:13 +0600

    arguments, bearl and cotinue for for loop

--- arguments.js
+++ arguments.js
@@ -1,6 +1,28 @@
 function add(num1, num2) {
-    console.log(arguments);
+    // console.log(arguments);
     return num1 + num2 + arguments[2];
 }
 const result = add(2, 3, 5, 10);
-console.log(result);
+// console.log(result);
+
+
+
+// break and continue for for loop
+
+// break means as like as stop the code
+var nums = [1, 2, 3, 4, 5, 6, 7];
+for (let i = 0; i < nums.length; i++) {
+    if (nums[i] > 3) {
+        break;
+    }
+    console.log(nums[i]);
+}
+console.log("a line break for easy to understand");
+// continue, continue means as like as skip
+var nums = [1, -2, -3, 4, -5, -6, 7];
+for (let i = 0; i < nums.length; i++) {
+    if (nums[i]<0) {
+        continue;
+    }
+    console.log(nums[i]);
+}
--- closure.js
+++ closure.js
@@ -1,9 +1,27 @@
 function sumAgain() {
     let count = 0;
-    return function() {
+    return function () {
         count++;
         return count;
     }
 }
 const result = sumAgain();
-console.log(result());
+// console.log(result());
+
+
+// array splice, slice, join
+const num = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+const slice = num.slice(2, 5);
+console.log(slice);
+
+const splice = num.splice(2, 5);
+console.log(splice);
+
+console.log(num);
+let num2 = [1, 2, 3, 4, 5, 6, 7];
+const join = num2.join(" next number ");
+console.log(join);
+const join2 = num2.join("");
+console.log(join2);
+const join3 = num2.join(",");
+console.log(join3);
